1492a22c9f68381e8ab0b3abc0ce036e7984affc
Nicolas Bouilleaud <nicolas.bouilleaud@gmail.com>
2013-01-03 16:25:05 +0100

    Custom Alert Icon

--- InstallSystemPlugins.m
+++ InstallSystemPlugins.m
@@ -11,6 +11,15 @@
 @interface InstallSystemPlugins : NSObject <NSApplicationDelegate>
 @end
 
+@interface NSAlert (InstallSystemPlugins) <NSAlertDelegate>
++ (NSInteger) confirmMoveWithMessage:(NSString*)messageText
+                     informativeText:(NSString*)informativeText
+                       defaultButton:(NSString*)defaultButton
+                            filename:(NSString*)filename
+                                 uti:(NSString*)uti
+                         installPath:(NSString*)installPath;
+@end
+
 int main(int argc, char *argv[])
 {
     NS_VALID_UNTIL_END_OF_SCOPE InstallSystemPlugins * delegate = [InstallSystemPlugins new];
@@ -26,14 +35,14 @@ int main(int argc, char *argv[])
 
 - (BOOL)application:(NSApplication *)sender openFile:(NSString *)filename
 {
+    // Activate App
+    [NSApp activateIgnoringOtherApps:YES];
     
 #pragma mark - Check Params
-
+    
     // Get basic info of opened document
     NSString * extension = [filename pathExtension];
     NSString * uti = (__bridge NSString *)(UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)(extension), NULL));
-    NSString * typeDescription = (__bridge NSString *)(UTTypeCopyDescription((__bridge CFStringRef)(uti)));
-    NSString * displayName = [[[NSFileManager defaultManager] displayNameAtPath:filename] stringByDeletingPathExtension];
     
     // Find document type declaration
     NSArray * types = [[NSBundle mainBundle] infoDictionary][@"CFBundleDocumentTypes"];
@@ -46,51 +55,57 @@ int main(int argc, char *argv[])
         }
     }];
     
-    NSString * installPath = typeDict[@"MSFInstallPath"];
+    NSString * installFolder = typeDict[@"ISPInstallPath"];
     
-    if([installPath length]==0)
+    if([installFolder length]==0)
         return NO;
     
     // Check wether the document we're opening is already in the destination folder
-    installPath = [[installPath stringByExpandingTildeInPath] stringByAppendingPathComponent:[filename lastPathComponent]];
+    NSString * installPath = [[installFolder stringByExpandingTildeInPath] stringByAppendingPathComponent:[filename lastPathComponent]];
     if([filename isEqualToString:installPath])
         [NSApp terminate:self];
     
     
-#pragma mark - Copy
-
-    // Copy
+#pragma mark - Move
+    
     NSError * error;
-    NSInteger alertResult;
     BOOL success;
-    
-    alertResult = [[NSAlert alertWithMessageText:[NSString stringWithFormat:NSLocalizedString(@"INSTALL_PLUGIN_%@_TYPE_%@", nil),displayName, typeDescription]
-                                   defaultButton:[NSString stringWithFormat:NSLocalizedString(@"BUTTON_INSTALL_%@", nil),displayName]
-                                 alternateButton:NSLocalizedString(@"CANCEL_BUTTON", nil)
-                                     otherButton:nil
-                       informativeTextWithFormat:NSLocalizedString(@"FILE_WILL_BE_MOVED_TO_%@", nil),filename, installPath] runModal];
-    BOOL shouldCopy = alertResult==NSAlertDefaultReturn;
-    if(!shouldCopy)
-        [NSApp terminate:self];
-    
-    // Handle "replace existing version"
-    success = [[NSFileManager defaultManager] copyItemAtPath:filename toPath:installPath error:&error];
-    if(!success && error.code == NSFileWriteFileExistsError)
-    {
-        alertResult = [[NSAlert alertWithMessageText:[NSString stringWithFormat:NSLocalizedString(@"PREVIOUS_VERSION_OF_%@_ALREADY_INSTALLED", nil),displayName]
-                                       defaultButton:NSLocalizedString(@"REPLACE_BUTTON", nil)
-                                     alternateButton:NSLocalizedString(@"CANCEL_BUTTON", nil)
-                                         otherButton:nil
-                           informativeTextWithFormat:NSLocalizedString(@"DO_YOU_WANT_TO_REPLACE", nil)] runModal];
-        BOOL shouldReplace = alertResult==NSAlertDefaultReturn;
-        if(!shouldReplace)
-            [NSApp terminate:self];
+    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:installPath];
 
-        success = [[NSFileManager defaultManager] removeItemAtPath:installPath error:&error];
-        success &= [[NSFileManager defaultManager] copyItemAtPath:filename toPath:installPath error:&error];
+    // Move the file
+    if( ! fileExists )
+    {
+        [NSAlert confirmMoveWithMessage:@"INSTALL_PLUGIN_%@_TYPE_%@"
+                        informativeText:@"FILE_WILL_BE_MOVED_TO_%@"
+                          defaultButton:@"BUTTON_INSTALL_%@"
+                               filename:filename
+                                    uti:uti
+                            installPath:installPath];
+        
+        // Move
+        success = [[NSFileManager defaultManager] moveItemAtPath:filename toPath:installPath error:&error];
     }
+
+    // File exists "now" : move failed, or we didn't attempt it
+    if(fileExists || (!success && error.code == NSFileWriteFileExistsError))
+    {
+        [NSAlert confirmMoveWithMessage:@"PREVIOUS_VERSION_OF_%@_OF_TYPE_%@_ALREADY_INSTALLED"
+                        informativeText:@"DO_YOU_WANT_TO_REPLACE_%@"
+                          defaultButton:@"BUTTON_REPLACE_%@"
+                               filename:filename
+                                    uti:uti
+                            installPath:installPath];
+        
+        success = [[NSFileManager defaultManager] removeItemAtPath:installPath error:&error];
+        success &= [[NSFileManager defaultManager] moveItemAtPath:filename toPath:installPath error:&error];
+    }
+    
+    // End with failure
     if(!success)
+    {
         [[NSAlert alertWithError:error] runModal];
+        [NSApp terminate:self];
+    }
     
     
 #pragma mark - Done
@@ -99,6 +114,70 @@ int main(int argc, char *argv[])
     
     [NSApp terminate:self];
     return YES;
+}
+
+@end
+
+/****************************************************************************/
+#pragma mark - UI Code \o/
+
+@implementation NSAlert (InstallSystemPlugins)
+
++ (NSInteger) confirmMoveWithMessage:(NSString*)messageText
+                     informativeText:(NSString*)informativeText
+                       defaultButton:(NSString*)defaultButton
+                            filename:(NSString*)filename
+                                 uti:(NSString*)uti
+                         installPath:(NSString*)installPath
+{
+    NSString * displayName = [[[NSFileManager defaultManager] displayNameAtPath:filename] stringByDeletingPathExtension];
+    NSString * typeDescription = (__bridge NSString *)(UTTypeCopyDescription((__bridge CFStringRef)(uti)));
+
+    NSAlert * alert = [self alertWithMessageText:[NSString stringWithFormat:NSLocalizedString(messageText,nil),displayName, typeDescription]
+                                   defaultButton:[NSString stringWithFormat:NSLocalizedString(defaultButton, nil), displayName]
+                                 alternateButton:NSLocalizedString(@"BUTTON_REVEAL_DESTINATION", nil)
+                                     otherButton:NSLocalizedString(@"CANCEL_BUTTON", nil)
+                       informativeTextWithFormat:[NSString stringWithFormat:NSLocalizedString(informativeText, nil), installPath],nil];
+
+    // Draw Icon
+    NSDictionary * typeDeclaration = (__bridge NSDictionary *)(UTTypeCopyDeclaration((__bridge CFStringRef)(uti)));
+    NSString * bundleID = typeDeclaration[(id)kCFBundleIdentifierKey];
+    NSString * appPath;
+    if (bundleID)
+        appPath = [[[NSWorkspace sharedWorkspace] URLForApplicationWithBundleIdentifier:bundleID] path];
+    if(appPath==nil)
+        appPath = [[NSBundle mainBundle] bundlePath];
+    NSImage * appIcon = [[NSWorkspace sharedWorkspace] iconForFile:appPath];
+    NSImage * fileIcon = [[NSWorkspace sharedWorkspace] iconForFile:filename];
+    NSBitmapImageRep* bitmap = [fileIcon representations][0];
+    CGSize size = CGSizeMake(bitmap.pixelsWide,bitmap.pixelsHigh);
+    fileIcon.size = appIcon.size = size;
+    [fileIcon lockFocus];
+    [appIcon drawInRect:NSMakeRect(size.width/2, 0,size.width/2, size.height/2) fromRect:NSMakeRect(0, 0, size.width, size.height) operation:NSCompositeSourceOver fraction:1];
+    [fileIcon unlockFocus];
+
+    alert.icon = fileIcon;
+    alert.showsHelp = YES;
+    alert.delegate = alert;
+    
+    
+    // Run
+    NSInteger result = [alert runModal];
+    
+    if(result==NSAlertAlternateReturn)
+    {
+        [[NSWorkspace sharedWorkspace] selectFile:[installPath stringByDeletingLastPathComponent] inFileViewerRootedAtPath:nil];
+        [NSApp terminate:self];
+    }
+
+    if(result==NSAlertOtherReturn)
+        [NSApp terminate:self];
+    return result;
+}
+
+- (BOOL)alertShowHelp:(NSAlert *)alert
+{
+    return [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:[[NSBundle mainBundle] infoDictionary][@"ISPHelpURL"]]];
 }
 
 @end
